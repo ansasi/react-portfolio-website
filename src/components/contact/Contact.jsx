4432e57dd79a712c6f447a72653005af96115215
ansas <ansasi1998@gmail.com>
2022-11-17 23:43:47 +0100

    keys hided

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,44 +1,57 @@
-import React, { useRef } from 'react';
-import './contact.css'
-import {HiOutlineMail} from 'react-icons/hi'
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import "./contact.css";
+import { HiOutlineMail } from "react-icons/hi";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_uazo04n', 'template_kxno83j', form.current, 'qlQtEUVdH18vc5E8l')
-    
-    e.target.reset()
+    emailjs.sendForm(
+      process.env.REACT_APP_EMAILJS_SERVICE_ID,
+      process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+      form.current,
+      process.env.REACT_APP_EMAILJS_USER_ID
+    );
+
+    e.target.reset();
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <HiOutlineMail className='contact__option-icon'/>
+            <HiOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>AngelSanchezSierra98@gmail.com</h5>
-            <a href="mailto:angelsanchezsierra98@gmail.com" target='_blank' rel="noreferrer">Send a message</a>
+            <a href="mailto:angelsanchezsierra98@gmail.com" target="_blank" rel="noreferrer">
+              Send a message
+            </a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
         <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name='name' placeholder='Your Full Name' required/>
-          <input type="email" name='email' placeholder='Your Email' required/>
-          <textarea name="message" cols="30" rows="7" placeholder='Your Message' required></textarea>
-          <button type="submit" className='btn btn-primary' value="Send">Send Message</button>
+          <input type="text" name="name" placeholder="Your Full Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            cols="30"
+            rows="7"
+            placeholder="Your Message"
+            required></textarea>
+          <button type="submit" className="btn btn-primary" value="Send">
+            Send Message
+          </button>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
